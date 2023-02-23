1e9bfa211dea59d598049f85dbe54c0c13d96684
Ismael Aceves <ismael@Ismaels-MacBook-Pro.local>
2023-02-23 15:42:43 -0500

    apply hide and show navbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import styles from "./navBar.module.css";
+
 
 //font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,21 +9,47 @@ import { faBars, faHome, faBorderAll } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
 
+    const [hideShowNav, setHideShowNav] = useState(false);
+
+    const onHideShowNav = () => {
+        setHideShowNav(!hideShowNav)
+    }
+
     return (
-        <nav className={styles.navContainer}>
-        <ul>
-            <li>
-                <div className={styles.iconContainer}><FontAwesomeIcon icon={faBars} /></div>
-            </li>
-          <li>
-            <Link to="/"><div className={styles.iconContainer}><FontAwesomeIcon icon={faHome}/></div> Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard"><div className={styles.iconContainer}><FontAwesomeIcon icon={faBorderAll}/></div> Dashboard</Link>
-            
-          </li>
-        </ul>
-      </nav>
+        <>
+            {
+                hideShowNav ?
+                    <nav className={styles.navContainer}>
+                        <ul>
+                            <li onClick={onHideShowNav}>
+                                <div className={styles.iconContainer}><FontAwesomeIcon icon={faBars} /></div>
+                            </li>
+                        <li>
+                            <Link to="/"><div className={styles.iconContainer}><FontAwesomeIcon icon={faHome}/></div> Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard"><div className={styles.iconContainer}><FontAwesomeIcon icon={faBorderAll}/></div> Dashboard</Link>
+                            
+                        </li>
+                        </ul>
+                    </nav>
+                :
+                    <nav className={styles.navContainer}>
+                        <ul>
+                            <li onClick={onHideShowNav}>
+                                <div className={styles.iconContainer}><FontAwesomeIcon icon={faBars} /></div>
+                            </li>
+                            <li>
+                                <Link to="/"><div className={styles.iconContainer}><FontAwesomeIcon icon={faHome}/></div></Link>
+                            </li>
+                            <li>
+                                <Link to="/dashboard"><div className={styles.iconContainer}><FontAwesomeIcon icon={faBorderAll}/></div></Link>
+                                
+                            </li>
+                        </ul>
+                    </nav>
+            }
+        </>
     )
 }
 
