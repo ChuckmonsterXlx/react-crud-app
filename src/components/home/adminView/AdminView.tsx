d46cd760a9d8aad59ef55d36a390271a48714e1a
Ismael Aceves <ismael@Ismaels-MacBook-Pro.local>
2023-03-08 17:28:11 -0500

    added option to change role status

--- src/components/home/adminView/AdminView.tsx
+++ src/components/home/adminView/AdminView.tsx
@@ -1,12 +1,84 @@
-import React, { useState } from "react";
-import { useAppSelector } from "../../../hooks/redux";
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { setUsers } from "../../../redux/slices/users";
+import { IRole } from "../../../redux/slices/verifedUser";
 import styles from "./adminView.module.css" 
 
 const AdminView = () => {
+    const dispatch = useAppDispatch();
+    const [visibleBtnUpdate, setVisibleBtnUpdate] = useState(true);
     const { users, verifedUser } = useAppSelector((state) => state);
+    const [roleOptionSelected, setRoleOptionSelected] = useState('');
     const [optionalRoles, setOptionalRoles] = useState([
         {label: "Admin", value: "admin"}, {label: "Member", value: "member"}
-    ])
+    ]);
+    
+    const [usersUpdate, setUsersUpdate] = useState<IUserUpdate[]>([]);
+
+    interface IUserUpdate {
+        userID: string,
+        role: string
+    }
+    const [userUpdate, setUserUpdate] = useState<IUserUpdate>({
+        userID: '',
+        role: ''
+    });
+
+    const handleSubmit = () => {
+        console.log(usersUpdate);
+        // const data = {
+        //     role: 
+        // };
+        // fetch("http://localhost:3001/users/" + , {
+        //   method: "PATCH",
+        //   headers: {
+        //     "Content-Type": "application/json"
+        //   },
+        //   body: JSON.stringify(data)
+        // })
+        // .then(res => res.json())
+        // .then(data => {
+        //   updated();
+        // })
+        // .catch(error => {
+        //   console.error('Ha ocurrido un error:', error);
+        // });
+    }
+
+    const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>, userID: string) => {
+        const selectedValue = event.target.value;
+        const selectedOption = optionalRoles.find((option) => option.value === selectedValue);
+        if (selectedOption) {
+          const userToUpdate = users.find((user) => user.id === userID);
+          if (userToUpdate) {
+            //if (usersUpdate.find((user) => user.userID === userID)) {
+            //    usersUpdate.push({userID: userID, role: selectedOption.value})
+            //}
+            setUserUpdate({...usersUpdate, userID: userID, role: selectedOption.value});
+            
+            const updatedUser = {
+              ...userToUpdate,
+              role: {
+                label: selectedOption.label,
+                value: selectedOption.value,
+              },
+            };
+            const updatedUsers = users.map((user) => {
+              if (user.id === userID) {
+                return updatedUser;
+              }
+              return user;
+            });
+            setUsersUpdate([...usersUpdate, { userID: userID, role: selectedOption.value }]);
+            dispatch(setUsers(updatedUsers));
+
+          }
+        }
+      };
+
+    useEffect(() => {
+        console.log(usersUpdate);
+    }, [usersUpdate])
 
     return (
         <>
@@ -27,7 +99,7 @@ const AdminView = () => {
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
                                     <td>
-                                        <select name="profileImg" value={user.role.value} onChange={() => console.log("Cambio")}>
+                                        <select name="role" value={user.role.value} onChange={(event) => handleRoleChange(event, user.id)}>
                                             {optionalRoles.map((option, index) => (
                                                 <option key={index} value={option.value}>
                                                     {option.label}
@@ -41,6 +113,15 @@ const AdminView = () => {
                     </tbody>
                 </table>
             </div>
+            {
+                visibleBtnUpdate ?
+                    <div>
+                        <button >Cancel</button>
+                        <button onClick={handleSubmit}>Update</button>
+                    </div>
+                :
+                    null
+            }
         </>
     )
 }
